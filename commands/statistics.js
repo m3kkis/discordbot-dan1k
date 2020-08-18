37072b8b38c218ab45aae8b07c6893f8e9731e32
Daniel Mekker <daniel.mekker@gmail.com>
2020-08-18 14:37:01 -0400

    added more stats and redesigned it

--- commands/statistics.js
+++ commands/statistics.js
@@ -14,8 +14,7 @@ module.exports = {
 
         if(args[0] == 'dealer')
         {
-            embedded.setDescription('Summary of the Dealer')
-                .addFields(
+            embedded.addFields(
                     { name:"Win", value: _Dealer.win, inline: true },
                     { name:"Loss", value: _Dealer.loss, inline: true },
                     { name:"Push", value: _Dealer.push, inline: true },
@@ -26,15 +25,66 @@ module.exports = {
         }
         else
         {
-            embedded.setDescription('Summary of the player')
-                .addFields(
-                    { name:"Win", value: _User.blackjack.win, inline: true },
-                    { name:"Loss", value: _User.blackjack.loss, inline: true },
-                    { name:"Push", value: _User.blackjack.push, inline: true },
-                    { name:"W/L", value: (_User.blackjack.win / _User.blackjack.loss), inline: true },
-                    { name:"Cash Won", value: "$" + _User.blackjack.cash_won, inline: true },
-                    { name:"Cash Lost", value: "$" + _User.blackjack.cash_lost, inline: true },
-                    { name:"Cash Spent", value: "$" + _User.blackjack.cash_spent, inline: true },
+            embedded.addFields(
+                    { 
+                        name:"Cash", 
+                        value: "$"+_User.economy.cash, 
+                        inline: true 
+                    },
+                    { 
+                        name:"Bank", 
+                        value: "$"+_User.economy.bank, 
+                        inline: true 
+                    },
+                    { 
+                        name:"Networth", 
+                        value: "$"+(_User.economy.cash + _User.economy.bank), 
+                        inline: true 
+                    },
+                    { 
+                        name:"Work", 
+                        value: "Success: " + _User.jobs.work.times_used_success + 
+                               "\nFailed: " + _User.jobs.work.times_used_failed + 
+                               "\nEarned: $" + _User.jobs.work.cash_earned +
+                               "\nLost: $" + _User.jobs.work.cash_lost,
+                        inline: true 
+                    },
+                    { 
+                        name:"Slut", 
+                        value: "Success: " + _User.jobs.slut.times_used_success + 
+                               "\nFailed: " + _User.jobs.slut.times_used_failed + 
+                               "\nEarned: $" + _User.jobs.slut.cash_earned +
+                               "\nLost: $" + _User.jobs.slut.cash_lost,
+                        inline: true 
+                    },
+                    { 
+                        name:"Crime", 
+                        value: "Success: " + _User.jobs.crime.times_used_success + 
+                               "\nFailed: " + _User.jobs.crime.times_used_failed + 
+                               "\nEarned: $" + _User.jobs.crime.cash_earned +
+                               "\nLost: $" + _User.jobs.crime.cash_lost,
+                        inline: true 
+                    },
+                    { 
+                        name:"BlackJack", 
+                        value: "Win: " + _User.blackjack.win + 
+                               "\nFailed: " + _User.blackjack.loss + 
+                               "\nPush: " + _User.blackjack.push +
+                               "\nW/L: " + (_User.blackjack.win / _User.blackjack.loss),
+                        inline: true 
+                    },
+                    { 
+                        name:"---", 
+                        value: "Cash made: $" + _User.blackjack.cash_won + 
+                               "\nCash lost: $" + _User.blackjack.cash_lost + 
+                               "\nCash spent: $" + _User.blackjack.cash_spent,
+                        inline: true 
+                    },
+                    { 
+                        name:"-----", 
+                        value: "\u200B",
+                        inline: true 
+                    },
                 );
         }
 
