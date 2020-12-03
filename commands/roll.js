51f737024db5cd72139e321d05d7cb0eced645ca
Daniel Mekker <daniel.mekker@gmail.com>
2020-12-02 20:04:24 -0500

    added custom max for roll

--- commands/roll.js
+++ commands/roll.js
@@ -7,16 +7,46 @@ module.exports = {
     usage: '',
     execute(client, message, args, _User){
 
+        var embedded = new Discord.MessageEmbed();
         console.log("[ROLL] Displaying roll result.");
 
-        var randomNbr = Math.floor(Math.random() * 100) + 1;
 
-        var embedded = new Discord.MessageEmbed();
-        embedded.setColor('#03b6fc')
-            .setAuthor(message.member.user.tag, message.member.user.avatarURL())
-            .setDescription(`You have rolled a **${randomNbr}**`);
+        if(args[0] != undefined)
+        {
+            var amount = Math.abs(parseInt(args[0]));
+
+            if( isNaN(amount) ) {
+                
+                embedded.setColor('#ff4f4f')
+                    .setDescription('That doesn\'t seem to be a valid NUMBER.');
+    
+                return message.channel.send(embedded);
+            }
+
+            var randomNbr = Math.floor(Math.random() * amount) + 1;
+
+            embedded.setColor('#03b6fc')
+                .setAuthor(message.member.user.tag, message.member.user.avatarURL())
+                .setDescription(`You have rolled a **${randomNbr}**`);
 
 
-        return message.channel.send(embedded);
+            return message.channel.send(embedded);
+        }
+        else
+        {
+            var randomNbr = Math.floor(Math.random() * 100) + 1;
+
+            embedded.setColor('#03b6fc')
+                .setAuthor(message.member.user.tag, message.member.user.avatarURL())
+                .setDescription(`You have rolled a **${randomNbr}**`);
+
+
+            return message.channel.send(embedded);
+
+
+        }
+
+
+
     }
 }
